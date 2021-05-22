6df73993765e970d1cad88769d3e65731855fa54
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-05-22 09:34:32 -0300

    Refec load

--- src/App.tsx
+++ src/App.tsx
@@ -4,42 +4,44 @@ import { uuid } from 'uuidv4'
 
 import { api } from './services/api';
 
-interface IData{
+interface IData {
   id: string;
   name: string;
   price: number;
 }
 
 const App: React.FC = () => {
-  const [ data, setData ] = useState<IData[]>([]);
-  const [ fruta, setFruta ] = useState<string>('')
-  const [ frutaValue, setFrutaValue ] = useState<any>()
+  const [data, setData] = useState<IData[]>([]);
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+  const [fruta, setFruta] = useState<string>('')
+  const [frutaValue, setFrutaValue] = useState<any>()
 
   useEffect(() => {
-    console.log(data)
+    console.log(isLoad)
     api.get('data').then(
       response => {
         setData(response.data)
       }
     )
-  }, [data]);
+  }, [isLoad]);
 
   const convertoToCurrency = useCallback(
     (value: number) => {
-      return Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL'})
-        .format(value / 100)
+      return Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' })
+        .format(value)
     }, []
   )
 
   const addToApi = useCallback(
-    () => { 
+    () => {
+      setIsLoad(true)
       api.post('data', {
         id: uuid,
         name: fruta,
         price: frutaValue
       }).then(
         response => alert('Tudo certo')
-      ).catch( e => alert('error'))
+      ).catch(e => alert('error')).finally(() => { setIsLoad(false) })
     }, [uuid, fruta, frutaValue]
   )
 
@@ -47,26 +49,31 @@ const App: React.FC = () => {
     <div>
       <h1>Hello</h1>
 
-        <ul>
-          { data.map( frut => (
-            <li key={frut.id}>
-              {frut.name} | {convertoToCurrency(frut.price)}
-            </li>
-          ))}
-        </ul>
-        <hr/>
-        <h1>{fruta}</h1>
-        <hr/>
-        <input type="text" 
-          onChange={ e => setFruta(e.target.value) } 
-          placeholder="Qual fruta" 
-        />
-        <input type="number" 
-          onChange={ e => setFrutaValue(parseFloat(e.target.value)) } 
-          placeholder="qual valor" 
-        />
-
-        <button onClick={ addToApi } >Adicionar</button>
+      <ul>
+        {data.map(frut => (
+          <li key={frut.id}>
+            {frut.name} | {convertoToCurrency(frut.price)}
+          </li>
+        ))}
+      </ul>
+      <hr />
+      { isLoad ? (
+        <div>
+          <p>Aguade, carregando....</p>
+        </div>
+      ) : (
+        <div>
+          <input type="text"
+            onChange={e => setFruta(e.target.value)}
+            placeholder="Qual fruta"
+          />
+          <input type="number"
+            onChange={e => setFrutaValue(parseFloat(e.target.value))}
+            placeholder="qual valor"
+          />
+          <button onClick={addToApi} >Adicionar</button>
+        </div>
+      )}
     </div>
   );
 }
